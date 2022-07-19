2ffd3268147b5bf4e38ec9477b5536d34a48bd36
Antonio Diêgo Silva <antoniodiegoluz@hotmail.com>
2022-07-18 23:36:28 -0300

    Primeiro commit

---
+++ src/br/antoniodiego/chat/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.antoniodiego.chat;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Antônio Diego
+ */
+public class Cliente extends
+        JFrame {
+
+    public Cliente() throws HeadlessException {
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
